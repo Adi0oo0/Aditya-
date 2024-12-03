ae669f602e3b2f95a5340e949ee65147395635b1
Aditya <adityasuryavanshi7711@gmail.com>
2024-12-03 14:55:49 +0530

    done

--- Data_Day_Wise.py
+++ Data_Day_Wise.py
@@ -2,7 +2,6 @@ import csv
 
 #Day,ScreenTime,NetEffectiveWork,SkillDev.(hrs.),Other,Feedback,Description
 
-
 class Handler:
     def __init__(self, file_name):
         self.file_name = file_name
@@ -12,6 +11,21 @@ class Handler:
             writer = csv.writer(file)
             writer.writerow(contents)
 
-    def take_inputs():
+    def take_inputs(self):
+        day = input('Enter Day: ')
         scrt = input('Enter ScreenTime: ')
-        neffwork = input
+        neffwork = input('Enter Net Effective Work: ')
+        skilldev = input('Enter Skill Development hours: ')
+        other = input('Enter Other activities: ')
+        feedback = input('Enter Feedback: ')
+        description = input('Enter Description: ')
+        
+        return [day, scrt, neffwork, skilldev, other, feedback, description]
+
+def main():
+    handler = Handler('daily_data.csv')
+    data = handler.take_inputs()
+    handler.write(data)
+
+if __name__ == "__main__":
+    main()
