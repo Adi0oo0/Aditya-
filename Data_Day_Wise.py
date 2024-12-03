4465c5efc857f928ec899dae8c880f251d8c8dbc
Aditya <adityasuryavanshi7711@gmail.com>
2024-12-03 15:04:44 +0530

    Done

--- Data_Day_Wise.py
+++ Data_Day_Wise.py
@@ -3,6 +3,13 @@ import csv
 #Day,ScreenTime,NetEffectiveWork,SkillDev.(hrs.),Other,Feedback,Description
 
 class Handler:
+    """
+    
+    This class handles the csv file
+    It has methods to write to the csv file and take inputs from the user
+    It also has a method to read the file.
+
+    """
     def __init__(self, file_name):
         self.file_name = file_name
     
@@ -12,6 +19,10 @@ class Handler:
             writer.writerow(contents)
 
     def take_inputs(self):
+        """
+        Returns:
+            list: csv data
+        """
         day = input('Enter Day: ')
         scrt = input('Enter ScreenTime: ')
         neffwork = input('Enter Net Effective Work: ')
@@ -23,6 +34,12 @@ class Handler:
         return [day, scrt, neffwork, skilldev, other, feedback, description]
 
 def main():
+    """
+    This is the main function
+    It creates an instance of the Handler class and calls the take_inputs method
+    It then calls the write method to write the data to the csv file
+
+    """
     handler = Handler('daily_data.csv')
     data = handler.take_inputs()
     handler.write(data)
